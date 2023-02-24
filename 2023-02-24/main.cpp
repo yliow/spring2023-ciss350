27d5ac6ea8801bf0bb0a0a9cf36157ccb3a08066
Yihsiang Liow <yliow@ccis.edu>
2023-02-24 12:40:24 -0500

    no msg

---
+++ 2023-02-24/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+// file: SLNode.h
+class SLNode
+{
+public:
+    SLNode(int key, SLNode * next=NULL)
+        : key_(key), next_(next)
+    {}
+//private:
+    int key_;
+    SLNode * next_;
+};
+
+std::ostream & operator<<(std::ostream & cout, const SLNode & n)
+{
+    cout << "<SLNode " << &n
+         << " key:" << n.key_
+         << ", next:" << n.next_
+         << ">";
+    return cout;
+}
+
+// file: SLList.h
+class SLList
+{
+public:
+    SLList()
+        : phead_(NULL)
+    {}
+    // Non-automatic resource (mem) is used. So need
+    // - destructor
+    // - copy constructor
+    // - operator=
+    void insert_head(int key)
+    {
+        SLNode * p = new SLNode(key);
+        p->next_ = phead_;
+        phead_ = p;
+    }
+    void delete_head(int key)
+    {
+        // TODO
+    }
+//private:
+    SLNode * phead_;
+    // int size_;
+};
+
+std::ostream & operator<<(std::ostream & cout, const SLList & list)
+{
+    cout << "<SLList " << &list << '\n';
+
+    SLNode * p = list.phead_;
+    while (p != NULL)
+    {
+        std::cout << (*p) << '\n';
+        p = p->next_;
+    }
+
+    cout << ">";
+    return cout;
+}
+
+int main()
+{
+    // 5 -> 3 -> 0 -> 1
+    SLNode * n5 = new SLNode(5);
+    SLNode * n3 = new SLNode(3);
+    SLNode * n0 = new SLNode(0);
+    SLNode * n1 = new SLNode(1);
+    n5->next_ = n3;
+    n3->next_ = n0;
+    n0->next_ = n1;
+
+    // std::cout << (*n5) << '\n';
+    // std::cout << (*n3) << '\n';
+    // std::cout << (*n0) << '\n';
+    // std::cout << (*n1) << '\n';
+
+    SLList list;
+    std::cout << list << '\n';
+    list.insert_head(1);
+    
+    delete n5;
+    delete n3;
+    delete n0;
+    delete n1;
+    return 0;
+}
